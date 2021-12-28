2784c2c1c7c14e25c1e237da74be7481ae4f9feb
ChanyCoding <95602922+ChanyCoding@users.noreply.github.com>
2021-12-28 18:01:27 +0900

    Add files via upload
    기본설정완료

---
+++ SpringMVC_JAVA/SpringMVC_JAVA/src/kr/co/study/config/ServletAppContext.java
@@ -0,0 +1,34 @@
+package kr.co.study.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// Spring MVC 프로젝트에 관련된 설정을 하는 클래스
+@Configuration
+// Controller 어노테이션이 셋팅되어있는 클래스를 Controller로 설정한다
+@EnableWebMvc
+// 스캔할 패키지 지정
+@ComponentScan("kr.co.study.controller")
+public class ServletAppContext implements WebMvcConfigurer{
+	
+	// controller에서 지정한 jsp파일 경로지정(suffix/prefix)
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.configureViewResolvers(registry);
+		registry.jsp("/WEB-INF/views/",".jsp");
+	}
+	
+	// 정적파일 경로지정
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+		registry.addResourceHandler("/**").addResourceLocations("/resources/");
+	}
+
+}
